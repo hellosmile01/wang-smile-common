eee4cac8801f157eee6cef5d21702097b9e1e4e1
smilewang <smilewangsy@outlook.com>
2018-08-31 00:40:13 +0800

    no message

--- smile-common/src/main/java/wang/smile/common/base/BaseResult.java
+++ smile-common/src/main/java/wang/smile/common/base/BaseResult.java
@@ -1,10 +1,13 @@
 package wang.smile.common.base;
 
+import lombok.Data;
+
 /**
  * 统一返回结果类
  * @author wangsy
  * @date 2018-5-26
  */
+@Data
 public class BaseResult {
 
     /**
@@ -22,34 +25,23 @@ public class BaseResult {
      */
     public Object data;
 
+    /**
+     * 列表数据
+     */
+    public Object rows;
+
+    public Integer total;
+
     public BaseResult(int code, String message, Object data) {
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
+    public BaseResult(int code, String message, Object rows, Integer total) {
         this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
+        this.rows = rows;
+        this.total = total;
     }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
 }
